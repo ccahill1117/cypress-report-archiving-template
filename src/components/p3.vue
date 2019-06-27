cf047928726134e40cf179522a01bed72d447a4d
Christopher Cahill <cthomascahill@gmail.com>
2019-06-27 10:42:46 -0700

    continue_section_3

--- src/components/p3.vue
+++ src/components/p3.vue
@@ -4,9 +4,21 @@
     <v-card>
       <v-card-text class="p0card">
         <p>3.1 - At the end of the last section, you might have noticed the last section of the buildspec.yml file is the post_build section. At that point, the 'npm run ci' line is run, which starts up a server at localhost:8080 in the CodeBuild virtual environment. Once the localhost:8080 is up and running, and serves our build and runs the the cypress tests located at cypress/integration.</p>
-        <img src="img/Screen Shot 2019-06-27 at 10.06.50 AM.png" class=walkthroughPix>
-
-        
+        <img src="img/Screen Shot 2019-06-27 at 10.26.34 AM.png" class=walkthroughPix>
+        <p>In this template, the buildspec.yml file is rewritten by the scripts/writeBuildspec.js file everytime the <code>$ npm run git "YOUR COMMIT MESSAGE"</code> command is run. This is achieved by use of the <a href="https://www.npmjs.com/package/simple-git"><strong>simple-git</strong></a> and <a href="https://www.npmjs.com/package/write-yaml"><strong>write-yaml</strong></a> packages.</p>
+        <p>Conveniently, this ensures that directory where your test results reside is given a name that timestamped and marked with the last commit message you made on your repository.</p>
+        <p>However, as mentioned, when using this process, you will want to make sure that your commit messages <strong>ONLY INCLUDE</strong> alphanumeric characters, spaces, dashes and underscores! Doing so will create an invalid directory name in S3. You can see in the picture <strong>above</strong> that the directory "2019-06-27_10-26_update_writeBuildspec.js" would actually fail due to the period.</p>
+        <img src="img/Screen Shot 2019-06-27 at 10.26.01 AM.png" class=walkthroughPix>
+      </v-card-text>
+    </v-card>
+  </div>
+  <div>
+    <v-card>
+      <v-card-text class="p0card">
+        <p>3.2 - However, we have not yet created a bucket for where our test results should be delivered by CodeBuild. We will want to do that now.</p> 
+        <p>Navigate to <a href="https://s3.console.aws.amazon.com/s3/">AWS S3 console</a> and create a bucket like below.</p>
+        <p>Make sure that you keep all your regions the same!</p>
+        <img src="img/Screen Shot 2019-06-27 at 10.37.23 AM.png" class=walkthroughPix>        
       </v-card-text>
     </v-card>
   </div>
