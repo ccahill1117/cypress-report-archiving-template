5f8f4af9dac2bf47020282ceca18ba331974cefa
Christopher Cahill <cthomascahill@gmail.com>
2019-06-28 13:40:31 -0700

    changing_up_tests

--- cypress/integration/admin/logout/loginLogout_spec.js
+++ cypress/integration/admin/logout/loginLogout_spec.js
@@ -1,7 +1,14 @@
-describe('Login and Logout', function() {
-  it('login and logout', function() {
-    cy.Login()
+describe('just testing', function() {
+  it('will just test a few basic things...', function() {
+
+    let userName = Cypress.env('USERNAME')
+    let pass = Cypress.env('PASSWORD')
+
     cy.visit('localhost:8080')
     cy.get('[data-cy=v-container]')
+    
+    cy.log(userName)
+    cy.log(pass)
+
   })
 })
